56c00a86ed37e70652780f5f8c9c937d03180fb0
Eliorika <arina.pavlovna.20@gmail.com>
2023-09-12 12:57:40 +0300

    Initial commit

---
+++ src/main/java/ru/rsreu/Babaian/ElectronicStore/HomeController.java
@@ -0,0 +1,18 @@
+package ru.rsreu.Babaian.ElectronicStore;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+/* Контроллер */
+
+public class HomeController {
+
+    @GetMapping("/")
+    /* Обрабатывает запросы корневого пути */
+    public String home() {
+        return "home"; /* Возвращает имя представления */
+    }
+}
